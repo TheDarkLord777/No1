3e71daa68560c9ee77baecb6571844806977b6bc
Maxmudbek <muzaffarovmahmudbek@gmail.com>
2024-09-06 12:46:20 +0500

    url change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header.jsx";
 
 function App() {
 const [data,setData]=useState(null)
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.API_URL;
 console.log(API_URL)
 
 useEffect(()=>{
