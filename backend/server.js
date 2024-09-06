ada5283dbe8420e852fe016b0be5e8b3214d92b6
Maxmudbek <muzaffarovmahmudbek@gmail.com>
2024-09-06 20:36:13 +0500

    changed

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ const PORT=process.env.PORT || 10000
 const cors = require('cors');
 const app=express()
 
-app.use(cors());
+app.use(cors({
+    origin: '*'  
+  }));
 
 app.listen(PORT,()=>{console.log(`Server starting on ${PORT}`)})
 app.get("/api",(req,res)=>{
