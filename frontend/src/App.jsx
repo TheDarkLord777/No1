4677d126d10f15dd01c92b7f60dfa78fa081aa5d
Maxmudbek <muzaffarovmahmudbek@gmail.com>
2024-09-07 17:12:19 +0500

    commited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
     <Container >
       <Header />
       <Inside>
-      <video src={myMedia} width={"100%"} height={"100%"}></video>
+      <video src={myMedia} width={"100%"} height={"100%"} loop autoPlay></video>
         </Inside>
       </Container>
   );
